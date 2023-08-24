1c308b117c80e944f1e62acb55c96b5e2c0a2122
Md. Jahid Hasan Jitu <124236705+jahidjitu-cse@users.noreply.github.com>
2023-08-24 22:52:03 +0600

    adding files

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
-import  express  from "express";
-import  jsonServer  from "json-server";
+import express from "express";
+import jsonServer from "json-server";
 import auth from "json-server-auth";
 
+const server = express();
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    next()
+})
 
-const server=express();
+const router = jsonServer.router('./data/db.json');
+server.use('/api', router);
+server.db = router.db
 
-server.use((req,res,next)=>{
-    res.header('Access-Control-Allow-Origin','*');
-    res.header('Access-Control-Allow-Header','*');
-    next();
+const middlewares = jsonServer.defaults()
+const rules = auth.rewriter({
+    products: 444,
+    featured_products: 444,
+    orders: 660,
+    users: 600
 });
 
-const router=jsonServer.router('./data/db.json');
-server.use('/api',router);
-server.db=router.db;
-
-const middlewares=jsonServer.defaults();
-
-const rules=auth.rewriter({
-    products:444,
-    feature_products:444,
-    orders:660,
-    users:600
-});
-
-server.use(rules);
-server.use(auth);
-server.use(middlewares);
-server.use(router);
+server.use(rules)
+server.use(auth)
+server.use(middlewares)
+server.use(router)
 
 server.listen(8000);
